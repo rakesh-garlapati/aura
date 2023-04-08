df26757a84fc5f3f39dfa306822aed2d60229608
rakesh garlapati <garlapatirakesh666@gmail.com>
2023-04-08 08:32:15 +0530

    calculator

--- force-app/main/default/aura/Aura_1/Aura_1.app
+++ force-app/main/default/aura/Aura_1/Aura_1.app
@@ -2,4 +2,5 @@
 <c:aura_1_lightningButton/>
 <c:aura_1_dataHandle/>
 <c:aura_1_input/>
+<c:aura_1_calculator/>
 </aura:application>	
---
+++ force-app/main/default/aura/aura_1_calculator/aura_1_calculatorController.js
@@ -0,0 +1,11 @@
+({
+    add : function(component, event, helper) {
+           aval = component.get("v.aval");
+           bval = component.get("v.bval");
+           result = parseaval +parsebval;
+           component.set("v.result",result);
+    },
+    sub : function(component, event, helper) {
+
+    }
+})
