014efcae6a647654fcbfffd73c33bcfabd426ed8
rakesh garlapati <garlapatirakesh666@gmail.com>
2023-04-08 08:40:32 +0530

    calculator edited

--- force-app/main/default/aura/aura_1_calculator/aura_1_calculatorController.js
+++ force-app/main/default/aura/aura_1_calculator/aura_1_calculatorController.js
@@ -1,8 +1,8 @@
 ({
     add : function(component, event, helper) {
-           aval = component.get("v.aval");
-           bval = component.get("v.bval");
-           result = parseaval +parsebval;
+           var aval = component.get("v.aval");
+          var  bval = component.get("v.bval");
+          var  result = parseInt(aval) +parseInt(bval);
            component.set("v.result",result);
     },
     sub : function(component, event, helper) {
