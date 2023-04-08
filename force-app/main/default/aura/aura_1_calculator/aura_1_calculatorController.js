a0ed4c408a31db83e73a75ce48373d2f6318157c
rakesh garlapati <garlapatirakesh666@gmail.com>
2023-04-08 08:41:46 +0530

    added subtration

--- force-app/main/default/aura/aura_1_calculator/aura_1_calculatorController.js
+++ force-app/main/default/aura/aura_1_calculator/aura_1_calculatorController.js
@@ -6,6 +6,9 @@
            component.set("v.result",result);
     },
     sub : function(component, event, helper) {
-
+        var aval = component.get("v.aval");
+        var  bval = component.get("v.bval");
+        var  result = parseInt(aval) - parseInt(bval);
+         component.set("v.result",result);
     }
 })
