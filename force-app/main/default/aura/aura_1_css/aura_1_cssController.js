bc88425245841e1325751ea9df956604eba52c0f
rakesh garlapati <garlapatirakesh666@gmail.com>
2023-04-08 16:21:47 +0530

    css

--- force-app/main/default/aura/aura_1_css/aura_1_cssController.js
+++ force-app/main/default/aura/aura_1_css/aura_1_cssController.js
@@ -1,11 +1,14 @@
 ({
     callme : function(component, event, helper) {
         var amount = component.get("v.amount");
-        var rate =component.get("v.rate");
-        var year =component.get("v.year");
-        var intrest = (parseInt(amount)*parseInt(rate)*parseInt(year))/100;
-if(intrest> 100){
-    component.get("v.flag",true);
-}
+        var rate =   component.get("v.rate");   
+        var year =   component.get("v.year");
+      var intrest = (amount*rate*year)/100;
+        component.set("v.intrest",intrest);
+if(intrest > 100){
+    component.set("v.flag",true);
+    }else{
+  component.set("v.flag",false);
+    }
     }
 })
