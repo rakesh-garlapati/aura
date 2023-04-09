e272d5812f1f7923e6caf79f0043e3357d8813cb
rakesh garlapati <garlapatirakesh666@gmail.com>
2023-04-09 23:44:48 +0530

    account class is edited

--- force-app/main/default/aura/aura_1_account/aura_1_accountController.js
+++ force-app/main/default/aura/aura_1_account/aura_1_accountController.js
@@ -10,5 +10,18 @@
             }
         });
         $A.enqueueAction(action);
+    },
+    getaccount : function(component, event, helper) {
+        var action = component.get("c.accountwitha");
+        
+        action.setCallback(this,function(response){
+            var state = response.getState();
+            if(state == 'SUCCESS'){
+                var res = response.getReturnValue();
+                component.set("v.accwitha",res);
+            }
+        });
+        $A.enqueueAction(action);
     }
+
 })
